bff2b5b7543a7900facd4f8bc79cb99d48a15e9d
Andi Fathul Mukminin <officialandifathul@gmail.com>
2024-08-16 14:11:29 +0700

    index page

--- myblog/views.py
+++ myblog/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    context_data = {}
+    context_data = {
+        'titles': ['Data Science 101', 'Programming 101', 'History of Discuss Throw',
+                   'Machine Learning 101', 'Advance Programming', 'Timurid Empire'],
+        'categories': ['Programming', 'Data Science', 'Machine Learning', 'Sports']
+    }
     return render(request, "index.html", context_data)
