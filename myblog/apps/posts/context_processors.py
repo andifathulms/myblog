ba1ee1a8eb5bc3cad1b0ccda9e3979819e5b5448
Andi Fathul Mukminin <officialandifathul@gmail.com>
2024-09-21 08:43:21 +0700

    update context processor

--- myblog/apps/posts/context_processors.py
+++ myblog/apps/posts/context_processors.py
@@ -1,12 +1,13 @@
 from django.http import HttpRequest
-from django.db.models import Count
+from django.db.models import Count, Q
 
-from .models import Category
+from .models import Category, Post
 from typing import Dict
 
 
 def post_settings(request: HttpRequest) -> Dict:
-    active_categories = Category.objects.annotate(post_count=Count('posts')) \
+    active_categories = Category.objects \
+        .annotate(post_count=Count('posts', filter=Q(posts__status=Post.STATUS.published))) \
         .filter(post_count__gt=0)
 
     return {
